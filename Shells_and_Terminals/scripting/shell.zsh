05e8887b481a434ada8e395cf8861543707a5c9c
SauravSinghKarmwar02 <100ravsinghkarmwar@gmail.com>
2024-04-19 14:26:14 +0530

    Learning Shells and Terminal from Boot.Dev

---
+++ Shells_and_Terminals/scripting/shell.zsh
@@ -0,0 +1,11 @@
+
+# Define variables
+bankname="WorldBanc"
+founded="1969"
+ceo="Jeff Gates"
+
+# Constructor output message using variables
+output="$bankname was founded in $founded by $ceo"
+
+# Print output message
+echo "$output"
